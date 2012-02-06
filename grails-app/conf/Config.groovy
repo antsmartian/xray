024b947594da0506c7e789f8c74f476aaa6744d3
Anto <anto.aravinth.cse@gmail.com>
2012-02-06 19:33:12 +0530

    hurry

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -11,8 +11,7 @@
 // }
 
 
-grails.plugin.cloudfoundry.username = "anto.aravinth.cse@gmail.com"
-grails.plugin.cloudfoundry.password = ""
+
 
 grails.project.groupId = appName // change this to alter the default package name and Maven publishing destination
 grails.mime.file.extensions = true // enables the parsing of file extensions from URLs into the request format
